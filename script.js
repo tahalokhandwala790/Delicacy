98eb3c12e85d2d3432f5f29330aaa76058026b0a
Tahalokhandwala790 <imgamerrr@gmail.com>
2024-07-26 22:39:44 +0530

    new change resposive

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ var swiper = new Swiper(".home", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  let menu = document.querySelector("#menu-icon");
+  let navbar = document.querySelector(".navbar");
+
+  menu.onclick = () =>{
+    menu.classList.toggle("bx-x")
+    navbar.classList.toggle("active")
+  }
+
+  window.onscroll = () =>{
+    menu.classList.remove("bx-x")
+    navbar.classList.remove("active")
+  }
