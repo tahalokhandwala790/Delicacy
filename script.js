6b7969d5723a61a5225e5da0f3c566ea1f84d7d3
Tahalokhandwala790 <imgamerrr@gmail.com>
2024-10-28 15:59:24 +0530

    commit1

--- script.js
+++ script.js
@@ -20,3 +20,15 @@ var swiper = new Swiper(".home", {
     menu.classList.remove("bx-x")
     navbar.classList.remove("active")
   }
+  const animate = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: '2500',
+    delay: '400',
+  })
+  
+  animate.reveal(".logo,.home-text1,.heading,.about-text",{origin: "left"});
+  animate.reveal(".about-img",{origin: "top"});
+  animate.reveal(".profile,.proimg1,.about-img-",{origin: "right"});
+  animate.reveal(".cSalad1,.cSalad2,.cSalad3",{origin: "bottom"});
+  animate.reveal(".navbar,.box,.about-text",{interval: 100});
